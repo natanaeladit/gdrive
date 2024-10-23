c67b41d64bcef9544032ab661127567b129da8c1
natanaeladit <nata.adit@gmail.com>
2024-10-24 07:45:25 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+
+Console.WriteLine("Connecting!");
+
+UserCredential credential;
+using (var stream = new FileStream("secrets.json", FileMode.Open, FileAccess.Read))
+{
+    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+        GoogleClientSecrets.Load(stream).Secrets,
+        new[] { DriveService.Scope.Drive },
+        "user", CancellationToken.None, new FileDataStore("Drive.ListMyLibrary"));
+}
+
+// Create the service.
+var service = new DriveService(new BaseClientService.Initializer()
+{
+    HttpClientInitializer = credential,
+    ApplicationName = "Drive API Sample",
+});
