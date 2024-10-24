c9622e591442bab2c8dd635ccb0380ba62685996
natanaeladit <nata.adit@gmail.com>
2024-10-25 05:49:38 +0800

    update code

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ using Google.Apis.Download;
 using Google.Apis.Upload;
 using System.Net.Mime;
 using Google;
+using System.Xml.Linq;
+using Google.Apis.Requests;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using System.Security.Cryptography.X509Certificates;
 
 
 Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
@@ -43,20 +48,32 @@ var builder = new ConfigurationBuilder()
                 .AddJsonFile("secrets.json", optional: false);
 IConfiguration config = builder.Build();
 
-ServiceAccountCredential credential = new ServiceAccountCredential(
-               new ServiceAccountCredential.Initializer(config["client_email"])
-               {
-                   Scopes = new[] { DriveService.Scope.DriveFile, DriveService.Scope.Drive }
-               }.FromPrivateKey(config["private_key"]));
+//Google Drive scopes Documentation:   https://developers.google.com/drive/web/scopes
+string[] scopes = new string[] {
+    DriveService.Scope.Drive,                   // view and manage your files and documents
+    DriveService.Scope.DriveAppdata,            // view and manage its own configuration data
+    DriveService.Scope.DriveAppsReadonly,       // view your drive apps
+    DriveService.Scope.DriveFile,               // view and manage files created by this app
+    DriveService.Scope.DriveMetadataReadonly,   // view metadata for files
+    DriveService.Scope.DriveReadonly,           // view files and documents on your drive
+    DriveService.Scope.DriveScripts };          // modify your app scripts
 
-// Create the service.
-var service = new DriveService(new BaseClientService.Initializer()
-{
-    HttpClientInitializer = credential,
-    ApplicationName = "Drive API Sample"
-});
+//var certificate = new X509Certificate2(config["certificate"]!, "notasecret", X509KeyStorageFlags.Exportable);
 
-var fileList = service.Files.List();
+//ServiceAccountCredential credential = new ServiceAccountCredential(
+//               new ServiceAccountCredential.Initializer(config["client_email"])
+//               {
+//                   Scopes = scopes,
+//               }.FromCertificate(certificate));
+
+//// Create the service.
+//var service = new DriveService(new BaseClientService.Initializer()
+//{
+//    HttpClientInitializer = credential,
+//    ApplicationName = "",
+//});
+
+var service = GetService();
 
 Console.WriteLine("Connected!");
 #endregion
@@ -71,7 +88,11 @@ Google.Apis.Drive.v3.Data.File uploadedFile = null;
 
 try
 {
-    await UploadFileAsync(service);
+    //await UploadFileAsync(service);
+
+    var uploadStream = new System.IO.FileStream(UploadFileName, System.IO.FileMode.Open,
+        System.IO.FileAccess.Read);
+    UploadFile(uploadStream, UploadFileName, ContentType, DownloadDirectoryName, UploadFileName);
 
     // uploaded succeeded
     Console.WriteLine("\"{0}\" was uploaded successfully", uploadedFile?.Name);
@@ -82,6 +103,70 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
+
+DriveService GetService()
+{
+    var tokenResponse = new TokenResponse
+    {
+        AccessToken = "",
+        RefreshToken = "",
+    };
+
+    var applicationName = ""; // Use the name of the project in Google Cloud
+    var username = ""; // Use your email
+
+    var apiCodeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+    {
+        ClientSecrets = new ClientSecrets
+        {
+            ClientId = "",
+            ClientSecret = ""
+        },
+        Scopes = scopes,
+        DataStore = new FileDataStore(applicationName)
+    });
+
+    var credential = new UserCredential(apiCodeFlow, username, tokenResponse);
+
+    var service = new DriveService(new BaseClientService.Initializer
+    {
+        HttpClientInitializer = credential,
+        ApplicationName = applicationName
+    });
+    return service;
+}
+
+Google.Apis.Drive.v3.Data.File CreateFile(DriveService service)
+{
+    var name = UploadFileName;
+    if (name.LastIndexOf('\\') != -1)
+    {
+        name = name.Substring(name.LastIndexOf('\\') + 1);
+    }
+    Google.Apis.Drive.v3.Data.File body = new Google.Apis.Drive.v3.Data.File { Name = name };
+    return service.Files.Create(body).Execute();
+}
+
+string UploadFile(Stream file, string fileName, string fileMime, string folder, string fileDescription)
+{
+    DriveService service = GetService();
+
+    var driveFile = new Google.Apis.Drive.v3.Data.File();
+    driveFile.Name = fileName;
+    driveFile.Description = fileDescription;
+    driveFile.MimeType = fileMime;
+    driveFile.Parents = new string[] { folder };
+
+    var request = service.Files.Create(driveFile, file, fileMime);
+    request.Fields = "id";
+
+    var response = request.Upload();
+    if (response.Status != Google.Apis.Upload.UploadStatus.Completed)
+        throw response.Exception;
+
+    return request.ResponseBody.Id;
+}
+
 async Task<IUploadProgress> UploadFileAsync(DriveService service)
 {
     var name = UploadFileName;
@@ -126,6 +211,34 @@ async Task DownloadFileAsync(DriveService service, string url)
             Console.WriteLine("Download {0} was interpreted in the middle. Only {1} were downloaded. ",
                 fileName, progress.BytesDownloaded);
         }
+    }
+}
+
+FileList ListFiles(DriveService service)
+{
+    // A page streamer is a helper to provide both synchronous and asynchronous page streaming
+    // of a listable or queryable resource.
+    var pageStreamer =
+        new PageStreamer<Google.Apis.Drive.v3.Data.File, FilesResource.ListRequest, FileList, string>(
+            (request, token) => request.PageToken = token,
+            response => response.NextPageToken, response => response.Files);
+
+    try
+    {
+        var results = new FileList();
+        var request = service.Files.List();
+
+        // iterate through the results
+        foreach (var result in pageStreamer.Fetch(request))
+        {
+            results.Files.Add(result);
+            Console.WriteLine(result?.Name);
+        }
+        return results;
+    }
+    catch (Exception ex)
+    {
+        throw new Exception($"Request Files.List failed with message {ex.Message}", ex);
     }
 }
 
